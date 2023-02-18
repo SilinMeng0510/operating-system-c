c0b23670336f853b4330b31ab3024763743b6afe
Silin Meng <Silinmeng@gmail.com>
2023-02-18 11:21:55 -0800

    fix bug that program reads newline into names

--- A1/countnames.c
+++ A1/countnames.c
@@ -10,15 +10,18 @@
 #include <string.h>
  
 
-int main()
+int main(int argc, char* argv[])
 {
-    FILE* file = fopen("names.txt", "r");
+    FILE *file = fopen(argv[1], "r");
     char line[256];
     char names[100][30];
     int occurence[100] = {0};
     int namesMap = 0;
+    int lineCounter = 1;
+
     while (fgets(line, sizeof(line), file)) {
         if (line[0] == '\n') {
+            fprintf(stderr, "Warning - Line %d is empty.\n", lineCounter);
             continue;
         }
         int found = 0;
@@ -34,6 +37,7 @@ int main()
             occurence[namesMap] = 1;
             namesMap++;
         }
+        ++lineCounter;
     }
     fclose(file);
     
@@ -43,6 +47,5 @@ int main()
         fprintf(output, "%s: %d\n", names[i], occurence[i]);
     }
 
-    fclose(output);
     return 0;
 }
