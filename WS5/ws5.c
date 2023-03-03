32702463b08ccd102e755a43606b8a9f9b2503c3
Silin Meng <Silinmeng@gmail.com>
2023-03-03 13:29:26 -0800

    complete ws5.c

---
+++ WS5/ws5.c
@@ -0,0 +1,66 @@
+/**
+
+ * Description: ws5.c: This code uses the fork() function to create a new 
+
+ *    process and then output hello world from PID followed by the process ID
+
+ * Author names: Silin Meng, Ibrahim Dobashi
+
+ * Author emails: silin.meng@sjsu.edu, ibrahim.dobashi@sjsu.edu
+
+ * Last modified date: 2/16/2023
+
+ * Creation date: 2/13/2023
+
+ **/
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include <sys/types.h>
+
+
+
+int main(){
+
+
+
+    pid_t pid;
+
+
+
+    pid = fork();
+
+    if (pid < 0) {     
+
+        fprintf(stderr, "fork failed\n");
+
+        exit(1);
+
+    } else if (pid == 0) { 
+
+        for (int i = 0; i < 4; i++) {
+
+            printf("hello world from PID %d!\n", getpid());
+
+        }
+
+    } else {         
+
+        for (int i = 0; i < 4; i++) {
+
+            printf("hello world from PID %d!\n", getpid());
+
+        }
+
+    }
+
+    return 0;
+
+}
+
