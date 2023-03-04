e5f76bf7533d9d04e10efefbef82e7c2c5c0ce67
Silin Meng <Silinmeng@gmail.com>
2023-03-03 16:41:20 -0800

    adding comments into the code

--- A2/countnames_parallel.c
+++ A2/countnames_parallel.c
@@ -14,14 +14,14 @@
 #define MAX_NAME 100
 #define MAX_LEN 30
 
-typedef struct my_data {
+typedef struct my_data {    // a structured data with 30 characters name and occurance count, named my_data
     char name[MAX_LEN];
     int count;
 } my_data;
 
 int count(FILE *file, struct my_data *namecounts){
 
-    char line[MAX_LEN]; 
+    char line[MAX_LEN]; //necessary local variable for reading file
     int namesMap = 0;
     int lineCounter = 1;
 
@@ -34,7 +34,7 @@ int count(FILE *file, struct my_data *namecounts){
         int found = 0;
         for (int i = 0; i < namesMap; i++) { // This for loop will check if the name is already been found
             char temp_line[MAX_LEN];
-            strncpy(temp_line, line, strlen(line) - 1);
+            strncpy(temp_line, line, strlen(line) - 1);  // check two version of input, with /n and without /n
             if (strcmp(namecounts[i].name, line) == 0 || strcmp(namecounts[i].name, temp_line) == 0) { // If yes, add occurence into the name
                 namecounts[i].count++;
                 found = 1;
@@ -54,7 +54,7 @@ int count(FILE *file, struct my_data *namecounts){
         ++lineCounter; // Count which line we are in
     }
 
-    return namesMap;
+    return namesMap;    // return how many name is recorded
 }
 
 
@@ -64,15 +64,15 @@ int main(int argc, char* argv[])
         exit(0);
     }
 
-    int pid;
-    struct my_data total_namecounts[MAX_NAME] = { { '\0', 0 } };
-    int total_count = 0;
-    int pfds[argc - 1][2];
-    
+    int pid;    // initial local variables, such as pid for fork()
+    struct my_data total_namecounts[MAX_NAME] = { { '\0', 0 } }; // my_data for result
+    int total_count = 0; // total_count for size of result
+    int pfds[argc - 1][2]; // pfds array for pipe between parent and different childs
+
     for(int i = 1; i < argc; ++i){
-        pipe(pfds[i-1]);
-        pid = fork();
-        if (pid < 0){
+        pipe(pfds[i-1]);    // pipe with one child each loop
+        pid = fork();   // fork new child process
+        if (pid < 0){   // if fork is not successful, return error
             fprintf(stderr, "error: can't fork");
         }
         else if (pid == 0) {
@@ -81,43 +81,41 @@ int main(int argc, char* argv[])
             int namesMap = 0;
             if (file == NULL) { // return error if file can't be opened
                 fprintf(stderr, "error: cannot open file %s\n", argv[i]);
-                write(pfds[i-1][1], &namesMap, sizeof(namesMap));
+                write(pfds[i-1][1], &namesMap, sizeof(namesMap));   // write nothing to parent for breaking loop
                 write(pfds[i-1][1], namecounts, sizeof(my_data) * MAX_NAME);
                 exit(1);
             }
-            namesMap = count(file, namecounts);
-            fclose(file);
-            write(pfds[i-1][1], &namesMap, sizeof(namesMap));
+            namesMap = count(file, namecounts); // implement function that return namecounts result of a file and number of result
+            fclose(file);   // close file
+            write(pfds[i-1][1], &namesMap, sizeof(namesMap)); // write results to parent through pipe
             write(pfds[i-1][1], namecounts, sizeof(my_data) * MAX_NAME);
             return 0;
         }
-        int pfds[2];
-        pipe(pfds);
     }
 
-    int pip_count = 0;
+    int pip_count = 0;  // count the pip corresponding to each child process
     while(wait(NULL) != -1){
-        struct my_data namecounts[MAX_NAME] = { { '\0', 0 } };
+        struct my_data namecounts[MAX_NAME] = { { '\0', 0 } }; // initialize new variable to store result from child
         int namesMap = 0;
-        read(pfds[pip_count][0], &namesMap, sizeof(namesMap));
+        read(pfds[pip_count][0], &namesMap, sizeof(namesMap));  // read result from child processes
         read(pfds[pip_count][0], namecounts, sizeof(my_data) * MAX_NAME);
 
-        for (int i = 0; i < namesMap; i++) {
+        for (int i = 0; i < namesMap; i++) {    // this for loop with combine result from child into the main result from parent
             int found = 0;
-            for (int j = 0; j < total_count; j++){
-                if (strcmp(total_namecounts[j].name, namecounts[i].name) == 0){
+            for (int j = 0; j < total_count; j++){  // this for loop will find if the name is already exist in main result of parent
+                if (strcmp(total_namecounts[j].name, namecounts[i].name) == 0){ // if found, combine the count
                     total_namecounts[j].count += namecounts[i].count;
                     found = 1;
                     break;
                 }
             }
-            if (!found){
+            if (!found){    // if not found, we will add it into the main result of parent
                 strcpy(total_namecounts[total_count].name, namecounts[i].name);
                 total_namecounts[total_count].count = namecounts[i].count;
                 total_count++;
             }
         }
-        pip_count++;
+        pip_count++;    // increment pip count for next child
     }
 
     for (int i = 0; i < total_count; i++) { // This for loop will print all the names and its occurence.
